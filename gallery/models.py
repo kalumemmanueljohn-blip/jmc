c03ef177dc60412996fe75c47f9d3bfbd8a4635a
kalumemmanueljohn-blip <kalumemmanueljohn@gmail.com>
2026-05-29 03:28:13 +0100

    Update models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 class GalleryCategory(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nom")
+    slug = models.SlugField(unique=True, blank=True, verbose_name="URL personnalisée")
     
     class Meta:
         verbose_name = "Catégorie"
         verbose_name_plural = "Catégories"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.name
@@ -13,6 +20,7 @@ class GalleryCategory(models.Model):
 
 class GalleryImage(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titre")
+    slug = models.SlugField(unique=True, blank=True, verbose_name="URL personnalisée")
     description = models.TextField(blank=True, verbose_name="Description")
     image = models.ImageField(upload_to='gallery/', verbose_name="Image")
     category = models.ForeignKey(GalleryCategory, on_delete=models.SET_NULL, null=True, blank=True)
@@ -20,12 +28,24 @@ class GalleryImage(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False, verbose_name="À la une")
     
+    # Comme le blog
+    author = models.CharField(max_length=100, verbose_name="Auteur", default='Administrateur')
+    views = models.IntegerField(default=0, verbose_name="Vues")
+    
     class Meta:
         verbose_name = "Image"
         verbose_name_plural = "Images"
-        ordering = ['-uploaded_at']
+        ordering = ['-taken_at', '-uploaded_at']
     
-
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            original_slug = self.slug
+            counter = 1
+            while GalleryImage.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.title
@@ -33,6 +53,7 @@ class GalleryImage(models.Model):
 
 class GalleryVideo(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titre")
+    slug = models.SlugField(unique=True, blank=True, verbose_name="URL personnalisée")
     description = models.TextField(blank=True, verbose_name="Description")
     video_url = models.URLField(blank=True, null=True, verbose_name="Lien YouTube/Vimeo")
     video_file = models.FileField(upload_to='gallery/videos/', blank=True, null=True, verbose_name="Fichier vidéo (MP4)")
@@ -41,11 +62,24 @@ class GalleryVideo(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False, verbose_name="À la une")
     
+    # Comme le blog
+    author = models.CharField(max_length=100, verbose_name="Auteur", default='Administrateur')
+    views = models.IntegerField(default=0, verbose_name="Vues")
+    
     class Meta:
         verbose_name = "Vidéo"
         verbose_name_plural = "Vidéos"
         ordering = ['-uploaded_at']
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            original_slug = self.slug
+            counter = 1
+            while GalleryVideo.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.title

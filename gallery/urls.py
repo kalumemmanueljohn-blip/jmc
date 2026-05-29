42ff15f58edafce38d0f3caa805bc501f5100db3
kalumemmanueljohn-blip <kalumemmanueljohn@gmail.com>
2026-05-29 03:30:16 +0100

    Update urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/ajouter/', views.add_photo, name='add_photo'),
     path('video/ajouter/', views.add_video, name='add_video'),
-    path('photo/supprimer/<int:id>/', views.delete_photo, name='delete_photo'),
-    path('video/supprimer/<int:id>/', views.delete_video, name='delete_video'),
+    path('photo/<slug:slug>/', views.photo_detail, name='photo_detail'),
+    path('video/<slug:slug>/', views.video_detail, name='video_detail'),
+    path('photo/supprimer/<slug:slug>/', views.delete_photo, name='delete_photo'),
+    path('video/supprimer/<slug:slug>/', views.delete_video, name='delete_video'),
 ]
